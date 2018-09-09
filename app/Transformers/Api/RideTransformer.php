f10fb1217fe609519d6408bc5cc10c41f4538507
matt trask <mjftrask@gmail.com>
2018-09-09 09:26:59 -0500

    see what ride is

--- app/Transformers/Api/RideTransformer.php
+++ app/Transformers/Api/RideTransformer.php
@@ -14,6 +14,7 @@ class RideTransformer extends TransformerAbstract
 
     public function transform(Ride $ride) : array
     {
+        dd($ride->detail);
         return [
             'id'                => $ride->getId(),
             'name'              => $ride->getName(),
